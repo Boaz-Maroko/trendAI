569c00bfb3649adb95ed884be2f1babbd1ff29c8
Boaz-Maroko <boazmaroko123@gmail.com>
2025-01-13 13:48:55 +0300

    backend build

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,23 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { VercelRequest, VercelResponse } from '@vercel/node';
 
-async function createApp() {
+async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Enable CORS for frontend communication
+  // Enable cors
   app.enableCors({
     origin: 'http://localhost:3000', // Replace with your frontend URL
     methods: 'GET,POST,PUT,DELETE,PATCH',
     credentials: true, // If using cookies or Authorization headers
   });
-
-  await app.init();
-  return app;
+  await app.listen(process.env.PORT ?? 3001);
 }
-
-// Vercel handler function
-export default async function handler(req: VercelRequest, res: VercelResponse) {
-  const app = await createApp();
-  app.getHttpAdapter().getInstance().handle(req, res);
-}
+bootstrap();
